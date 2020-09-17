0253247b00a4c66279872c0350b365793ddf7d73
Holger Lösken <hl@inquence.com>
2020-09-17 22:07:54 +0200

    Add missing translations
    Fix #1

--- resources/lang/de/cp.php
+++ resources/lang/de/cp.php
@@ -1,7 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
 return [
     'settings' => [
-        'headline' => 'Einstellungen',
+        'headline' => 'MagicLink',
+        'configure_your_needs' => 'Passe MagicLink nach deinen Wünschen an',
+        'ml_enabled' => 'MagicLink aktivieren?',
+        'ml_enabled_instructions' => 'Aktiviere oder deaktiviere die Nutzung von MagicLinks',
+        'ml_expire_time' => 'Ablaufzeit',
+        'ml_expire_time_instructions' => 'Laufzeit des Links, nachdem er verfällt. Standart sind 30 Minuten.',
+        'updated_successfully' => 'Einstellungen erfolgreich aktualisiert.',
     ],
+    'permissions' => [
+        'settings' => 'MagicLink Einstellung',
+        'view_settings' => 'Einstellungen ansehen',
+        'view_settings_description' => 'Zugriff, um Einstellungen anzusehen.',
+    ],
+    'email' => [
+        'new_link_subject' => 'Ihr Login-Link',
+    ],
+
+    'unable_to_save' => 'Einstellungen nicht gespeichert. Bitte passe die fehlerhaften Felder an und speichere erneut.',
+
 ];
